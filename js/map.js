9ee053540251b21d60f02db33dc8df6bd9418a01
danactive <githubcom@domaindesign.ca>
2014-07-13 00:05:57 -0700

    Harness > Map: Separate map provider with app logic

--- js/map.js
+++ js/map.js
@@ -1,56 +1,85 @@
 /*global mxn, requireArg, window*/
 window.historyApp = {
 	"current": {
-		"locationIndex": 0
+		"itemIndex": 0
 	},
-	"map": {
-		"location": []
-	}
+	"items": [],
+	"lookup": {}
 };
 
-function Map(options) {
+function MapProvider(options) {
 	"use strict";
+
 	function init() {
-		var centrePoint = new mxn.LatLonPoint(centreGeoCode[1], centreGeoCode[0]);
+		var centrePoint = new mxn.LatLonPoint(options.centreCoordinates[1], options.centreCoordinates[0]);
 		
-		slippyMap = new mxn.Mapstraction(mapContainer, mapProvider);
+		slippyMap = new mxn.Mapstraction(options.mapContainer, "leaflet");
 		slippyMap.setCenterAndZoom(centrePoint, 10);
 		slippyMap.addControls({ zoom: 'large', map_type: true });
 		slippyMap.setMapType(1); // 1 = Street, 2 Satellite
 		slippyMap.enableScrollWheelZoom();
 	}
+
+	var slippyMap;
+
+	this.pin = {};
+	this.pin.add = function (args) {
+		var pushpin = new mxn.Marker(new mxn.LatLonPoint(args.coordinates[1], args.coordinates[0]));
+		pushpin.setInfoBubble(args.id);
+		slippyMap.addMarker(pushpin);
+		return pushpin;
+	};
+
+	this.pin.select = function (pushpin) {
+		pushpin.openBubble();
+	};
+
+	init();
+}
+
+function Map(options) {
+	"use strict";
+
 	var cache = window.historyApp,
-		centreGeoCode = requireArg({"args": options, "name": "centre", "type": "array"}),
+		centreCoordinates = requireArg({"args": options, "name": "centre", "type": "array"}),
 		mapContainer = requireArg({"args": options, "name": "container", "type": "string"}),
-		mapProvider = "leaflet",
-		slippyMap;
+		mapProvider;
+
+	mapProvider = new MapProvider({
+		"centreCoordinates": centreCoordinates,
+		"mapContainer": mapContainer
+	});
 
 	this.pin = {};
 	this.pin.add = function (args) {
 		var coordinates = requireArg({"args": args, "name": "coordinates", "type": "array"}),
-			open = requireArg({"args": args, "name": "open", "type": "boolean"}),
+			id = requireArg({"args": args, "name": "id", "type": "string"}),
+			selected = requireArg({"args": args, "name": "selected", "type": "boolean"}),
 			pushpin;
 
-		pushpin = new mxn.Marker(new mxn.LatLonPoint(coordinates[1], coordinates[0]));
-		pushpin.setInfoBubble(coordinates[1]);		
-		slippyMap.addMarker(pushpin);
+		pushpin = mapProvider.pin.add({
+			"coordinates": coordinates,
+			"id": id
+		});
+		
+		cache.lookup[id] = { "pin": pushpin };
+		cache.items.push(id);
 
-		cache.map.location.push({ "pin": pushpin });
-
-		if (open === true) {
-			pushpin.openBubble();
+		if (selected === true) {
+			mapProvider.pin.select(pushpin);
 		}
 	};
+
 	this.pin.next = function () {
-		var carouselBeginAgain = (cache.map.location.length - 1 === cache.current.locationIndex);
-		cache.current.locationIndex++;
+		var carouselBeginAgain = (cache.items.length - 1 === cache.current.itemIndex),
+			currentId;
+		cache.current.itemIndex++;
 		if (carouselBeginAgain) {
-			cache.current.locationIndex = 0;
+			cache.current.itemIndex = 0;
 		}
-		cache.map.location[cache.current.locationIndex].pin.openBubble();
+		currentId = cache.items[cache.current.itemIndex];
+		mapProvider.pin.select(cache.lookup[currentId].pin);
 	};
-
-	init();
 
 	return this;
 }
