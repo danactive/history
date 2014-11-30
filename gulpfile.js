0a4223cf3e97c05100c5104287cebbc71269ffde
danactive <githubcom@domaindesign.ca>
2014-11-29 22:13:08 -0800

    Add watch for xml2json

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-var gulp = require('gulp'),
+var getGalleries = require('./admin/get_gallery_directories.js'),
+	gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')({"camelize": true});
 
 gulp.task('xml2json', function () {
-	var getGalleries = require('./admin/get_gallery_directories.js');
+	var DEST,
+		stream;
 
 	getGalleries().galleries.forEach(function (name) {
-		gulp.src('gallery-' + name + '/xml/*.xml')
+		DEST = 'gallery-' + name + '/json';
+		stream = gulp.src('gallery-' + name + '/xml/*.xml')
+			.pipe(plugins.changed(DEST))
 			.pipe(plugins.xml2json())
 			.pipe(plugins.rename({"extname": '.json'}))
-			.pipe(gulp.dest('gallery-' + name + '/json'));
+			.pipe(gulp.dest(DEST));
 	});
+
+	return stream;
+});
+
+gulp.task('watch', function() {
+	var stream;
+	getGalleries().galleries.forEach(function (name) {
+		stream = gulp.watch('gallery-' + name + '/xml/*.xml', ['xml2json']);
+	});
+
+	return stream;
 });
