34d0bc9b28ae1b499a7a371b7029abb6810e6056
Dan BROOKS <githubcom@domaindesign.ca>
2016-11-08 20:53:13 -0800

    fix(Test): Enable resize and measure image

--- plugins/resize/test/resize.js
+++ plugins/resize/test/resize.js
@@ -35,7 +35,7 @@ tape('Verify resize library', { skip: false }, (describe) => {
       });
   });
 
-  describe.test('* Resize JPEG file to photo and thumb to parent folder', { skip: true }, (assert) => {
+  describe.test('* Resize JPEG file to photo and thumb to parent folder', { skip: false }, (assert) => {
     const originalRelativeFile = path.join(fixturesPath, 'originals/2016-07-12.jpg');
     plugin.resize(originalRelativeFile)
       .then(() => {
@@ -50,30 +50,6 @@ tape('Verify resize library', { skip: false }, (describe) => {
           });
 
         const thumbPath = originalAbsoluteFile.replace(ORIGINAL_FOLDER_NAME, THUMB_FOLDER_NAME);
-        calipers.measure(thumbPath)
-          .then((result) => {
-            assert.equal(result.pages[0].width, thumbDims.width, 'Thumb width measured');
-            assert.ok(result.pages[0].height <= thumbDims.height, 'Thumb height measured');
-          });
-      })
-      .catch(error => assert.fail(error));
-  });
-
-  describe.test('* Resize JPEG folder to photo and thumb to parent folder', { skip: true }, (assert) => {
-    const originalRelativeFolder = path.join(fixturesPath, '2016-07-12.jpg');
-    plugin.resize(originalRelativeFolder)
-      .then((originalFiles) => {
-        assert.plan(4);
-        // todo: Finish testing folder using glob to find all JPG files
-        const originalAbsoluteFolder = path.resolve(__dirname, '../../../', originalFiles);
-        const photoPath = originalAbsoluteFolder.replace(ORIGINAL_FOLDER_NAME, PHOTO_FOLDER_NAME);
-        calipers.measure(photoPath)
-          .then((result) => {
-            assert.equal(result.pages[0].width, photoDims.width, 'Photo width measured');
-            assert.ok(result.pages[0].height <= photoDims.height, 'Photo height measured');
-          });
-
-        const thumbPath = originalAbsoluteFolder.replace(ORIGINAL_FOLDER_NAME, THUMB_FOLDER_NAME);
         calipers.measure(thumbPath)
           .then((result) => {
             assert.equal(result.pages[0].width, thumbDims.width, 'Thumb width measured');
