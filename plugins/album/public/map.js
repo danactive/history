c083190056e519b5da39798c5997c13ef4a0d7ec
danactive <danactive@users.noreply.github.com>
2016-11-16 22:31:23 -0800

    feat(Map Album): Plot markers with thumb icon, center to first marker (#40)

--- plugins/album/public/map.js
+++ plugins/album/public/map.js
@@ -1,59 +1,27 @@
 /* global document, mapboxgl, window */
-function createMap() {
-  const geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        properties: {
-          message: 'Foo',
-          iconSize: [60, 60],
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [
-            -66.324462890625,
-            -16.024695711685304,
-          ],
-        },
-      },
-      {
-        type: 'Feature',
-        properties: {
-          message: 'Bar',
-          iconSize: [50, 50],
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [
-            -61.2158203125,
-            -15.97189158092897,
-          ],
-        },
-      },
-      {
-        type: 'Feature',
-        properties: {
-          message: 'Baz',
-          iconSize: [40, 40],
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [
-            -63.29223632812499,
-            -18.28151823530889,
-          ],
-        },
-      },
-    ],
-  };
+function createMap(response) {
+  const ICON_SQUARE_PX = 60;
+  const hasGeo = item => item.geo;
+  const schema20ToGeoJson = item => ({
+    type: 'Feature',
+    properties: {
+      caption: item.caption,
+      thumbPath: item.thumbPath,
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [item.geo.lon, item.geo.lat],
+    },
+  });
+  const geojson = { type: 'FeatureCollection', features: [] };
+
+  geojson.features = response.album.items.filter(hasGeo).map(schema20ToGeoJson);
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuYWN0aXZlIiwiYSI6ImNpdmo0OGo2YTAxcGIyenBkZWZlN3Ewam4ifQ.npY0cY_HdHg1OB692HtcUw';
   const map = new mapboxgl.Map({
     container: 'mapBox', // container id
     style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-    center: [-66.324462890625, -16.024695711685304], // starting position
-    zoom: 3, // starting zoom
+    zoom: response.album.meta.geo.googleZoom,
   });
 
   // add markers to map
@@ -61,19 +29,21 @@ function createMap() {
     // create a DOM element for the marker
     const el = document.createElement('div');
     el.className = 'marker';
-    el.style.backgroundImage = `url(https://placekitten.com/g/${marker.properties.iconSize.join('/')}/)`;
-    el.style.width = `${marker.properties.iconSize[0]}px`;
-    el.style.height = `${marker.properties.iconSize[1]}px`;
+    el.style.backgroundImage = `url(${marker.properties.thumbPath})`;
+    el.style.width = `${ICON_SQUARE_PX}px`;
+    el.style.height = `${ICON_SQUARE_PX}px`;
 
     el.addEventListener('click', () => {
-      window.alert(marker.properties.message);
+      map.panTo(marker.geometry.coordinates);
     });
 
     // add marker to map
-    new mapboxgl.Marker(el, { offset: [-marker.properties.iconSize[0] / 2, -marker.properties.iconSize[1] / 2] })
+    new mapboxgl.Marker(el, { offset: [-ICON_SQUARE_PX / 2, -ICON_SQUARE_PX / 2] })
       .setLngLat(marker.geometry.coordinates)
       .addTo(map);
   });
+
+  map.panTo(geojson.features[0].geometry.coordinates);
 }
 
 // If Node.js then export as public
