090a8994add2a2ce0159b290525f06b79c08371f
Dan BROOKS <githubcom@domaindesign.ca>
2016-12-10 17:09:50 -0800

    fix(Lint): Blank test corrected [#66]

--- plugins/admin/test/index.spec.js
+++ plugins/admin/test/index.spec.js
@@ -4,6 +4,8 @@ tape('Verify /admin route', { skip: false }, (describe) => {
   const lib = require('../lib');
 
   describe.test('* Placeholder', { skip: false }, (assert) => {
+    assert.ok(lib);
+
     assert.end();
   });
 });
