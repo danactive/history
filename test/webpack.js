d77f30bc6e2bd126c33573db4b35814a10003054
danactive <danactive@users.noreply.github.com>
2017-02-04 15:05:24 -0800

    test(Repo): Add test for webpack build step [#115]

---
+++ test/webpack.js
@@ -0,0 +1,6 @@
+const test = require('tape');
+
+test('successful test', function (t) {
+  t.equal(1, 1)
+  t.end()
+})
--- webpack.js
+++ webpack.js
@@ -1,25 +1,42 @@
-module.exports = {
-  entry: {
-    album: './plugins/album/lib/client.js',
-    editAlbum: './plugins/editAlbum/lib/client.js',
-  },
-  resolve: {
-    extensions: ['', '.css', '.js', '.jsx'],
-  },
-  output: {
-    filename: './plugins/[name]/public/assets/bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
-        },
-      },
-      { test: /\.css$/, loader: 'style!css' },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url-loader?limit=100000' },
+const TapWebpackPlugin = require('tap-webpack-plugin');
+
+module.exports = [
+  // test bundle configuration
+  {
+    target: 'node',
+    entry: './test/webpack',
+    output: {
+      path: 'tmp',
+      filename: 'test.js',
+    },
+    plugins: [
+      new TapWebpackPlugin(),
     ],
   },
-};
+  // export npm modules to browser scripts
+  {
+    entry: {
+      album: './plugins/album/lib/client.js',
+      editAlbum: './plugins/editAlbum/lib/client.js',
+    },
+    resolve: {
+      extensions: ['', '.css', '.js', '.jsx'],
+    },
+    output: {
+      filename: './plugins/[name]/public/assets/bundle.js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015'],
+          },
+        },
+        { test: /\.css$/, loader: 'style!css' },
+        { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url-loader?limit=100000' },
+      ],
+    },
+  },
+];
