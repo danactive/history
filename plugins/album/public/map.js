4924c83d510a9dc6e6469fcaa8c98be786bcae9b
danactive <danactive@users.noreply.github.com>
2017-08-20 09:58:17 -0700

    feat(Map): Add marker for unclustered geo locations [#37]

--- plugins/album/public/map.js
+++ plugins/album/public/map.js
@@ -12,66 +12,63 @@ function createMap(containerId) {
     const albumStem = getQueryByName('album_stem');
     const gallery = getQueryByName('gallery');
 
-    // Add a new source from our GeoJSON data and set the
-    // 'cluster' option to true.
-    window.map.addSource('earthquakes', {
+    window.map.addSource('poi', {
       type: 'geojson',
-      // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-      // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
       data: `/geojson?album_stem=${albumStem}&gallery=${gallery}`,
       cluster: true,
       clusterMaxZoom: 14, // Max zoom to cluster points on
       clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
     });
 
-    // Use the earthquakes source to create five layers:
-    // One for unclustered points, three for each cluster category,
-    // and one for cluster labels.
     window.map.addLayer({
-      id: 'unclustered-points',
-      type: 'symbol',
-      source: 'earthquakes',
-      filter: ['!has', 'point_count'],
-      layout: {
-        'icon-image': 'marker-15'
+      id: 'clusters',
+      type: 'circle',
+      source: 'poi',
+      filter: ['has', 'point_count'],
+      paint: {
+        'circle-color': {
+          property: 'point_count',
+          type: 'interval',
+          stops: [
+            [0, '#51bbd6'],
+            [100, '#f1f075'],
+            [750, '#f28cb1']
+          ]
+        },
+        'circle-radius': {
+          property: 'point_count',
+          type: 'interval',
+          stops: [
+            [0, 20],
+            [100, 30],
+            [750, 40]
+          ]
+        }
       }
     });
 
-    // Display the earthquake data in three layers, each filtered to a range of
-    // count values. Each range gets a different fill color.
-    const layers = [
-      [150, '#f28cb1'],
-      [20, '#f1f075'],
-      [0, '#51bbd6']
-    ];
-
-    layers.forEach((layer, i) => {
-      window.map.addLayer({
-        id: `cluster-${i}`,
-        type: 'circle',
-        source: 'earthquakes',
-        paint: {
-          'circle-color': layer[1],
-          'circle-radius': 18
-        },
-        filter: i === 0 ?
-          ['>=', 'point_count', layer[0]] :
-          ['all', ['>=', 'point_count', layer[0]], ['<', 'point_count', layers[i - 1][0]]]
-      });
-    });
-
-    // Add a layer for the clusters' count labels
     window.map.addLayer({
       id: 'cluster-count',
       type: 'symbol',
-      source: 'earthquakes',
+      source: 'poi',
+      filter: ['has', 'point_count'],
       layout: {
-        'text-field': '{point_count}',
-        'text-font': [
-          'DIN Offc Pro Medium',
-          'Arial Unicode MS Bold'
-        ],
+        'text-field': '{point_count_abbreviated}',
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
         'text-size': 12
+      }
+    });
+
+    window.map.addLayer({
+      id: 'unclustered-point',
+      type: 'circle',
+      source: 'poi',
+      filter: ['!has', 'point_count'],
+      paint: {
+        'circle-color': '#11b4da',
+        'circle-radius': 8,
+        'circle-stroke-width': 1,
+        'circle-stroke-color': '#fff'
       }
     });
   });
