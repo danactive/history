4e3db4b1602fd8c7381c42b423d5ff31b0adcde9
danactive <dan@domaindesign.ca>
2018-08-12 21:47:27 -0700

    test(Slippy Map): Add test for Mapbox container

---
+++ ui/app/containers/SlippyMap/tests/index.jest.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { shallow } from 'enzyme';
+
+/* global expect, test */
+
+test('<SlippyMap />', () => {
+  import { SlippyMap } from '../index';
+
+  expect(lib);
+});
