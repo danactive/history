6f3187a48de557c54e2e700a3d00638fdfe66f42
danactive <dan@domaindesign.ca>
2019-02-17 21:10:27 -0800

    fix(Album View): Widen column to fullscreen

--- ui/app/containers/App/index.jsx
+++ ui/app/containers/App/index.jsx
@@ -14,7 +14,6 @@ import Footer from '../../components/Footer';
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
   min-height: 100%;
