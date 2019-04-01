00d3d01322104f90a516482962eccfa04d84421b
danactive <dan@domaindesign.ca>
2019-03-31 18:52:23 -0700

    test(Admin): Convert unit test assertion library from tape to jest

--- server/plugins/admin/test/page.jest.jsx
+++ server/plugins/admin/test/page.jest.jsx
@@ -1,23 +1,16 @@
-import Adapter from 'enzyme-adapter-react-16';
-import enzyme from 'enzyme';
+/* global describe, expect, shallow, test */
 import React from 'react';
-import test from 'tape';
 
-import '../../../test/setup.enzyme';
 import Page from '../components/page';
 
-test('View Admin - Page (React Component)', { skip: false }, (describe) => {
-  const { shallow } = enzyme;
-
-  enzyme.configure({ adapter: new Adapter() });
-
-  describe.test('* Page loads', (assert) => {
+describe('View Admin - Page (React Component)', () => {
+  test('* Page loads', () => {
     const wrapper = shallow(<Page />);
-    assert.ok(wrapper.contains(
+
+    expect(wrapper.contains(
       <a href="/edit/album">
         Edit Album
       </a>,
-    ));
-    assert.end();
+    )).toBeTruthy();
   });
 });
