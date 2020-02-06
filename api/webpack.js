020b2f719d611735f1418972011eaff141e1e5b6
danactive <dan@domaindesign.ca>
2020-02-05 22:47:54 -0800

    feat(Explore Video): Move YouTube player components from API to UI

--- api/webpack.js
+++ api/webpack.js
@@ -21,7 +21,6 @@ module.exports = [
     entry: {
       album: './server/plugins/album/lib/browser.js',
       editAlbum: './server/plugins/editAlbum/lib/browser.js',
-      exploreVideo: './server/plugins/exploreVideo/lib/browser.js',
       walk: './server/plugins/walk/lib/browser.js',
     },
     resolve: {
