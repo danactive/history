8d4175704750a24a3e350d4162512e11139ae71b
danactive <dan@domaindesign.ca>
2020-02-12 17:48:29 -0800

    fix(Resize): Improve error message when GraphicsMagick is missing

--- api/server/plugins/resize/lib/index.js
+++ api/server/plugins/resize/lib/index.js
@@ -1,13 +1,22 @@
+const dotProp = require('dot-prop');
+
 const resizeMod = require('./resize');
 const validation = require('../../../lib/validation');
 
-const handler = request => new Promise((reply) => {
+async function handler(request, h) {
   const sourcePath = request.payload.source_path;
 
-  resizeMod.resize(sourcePath)
-    .then(() => reply({ resize: true }))
-    .catch(reply);
-});
+  try {
+    const response = await resizeMod.resize(sourcePath);
+    if (dotProp.get(response, 'meta.error.count', 0) > 0) {
+      return h.response(response.meta.error).code(500);
+    }
+
+    return { resize: true };
+  } catch (error) {
+    return error;
+  }
+}
 
 const register = (server) => {
   server.route({
@@ -21,11 +30,6 @@ const register = (server) => {
           source_path: validation.sourceFolder,
         },
       },
-      response: {
-        schema: {
-          resize: validation.resize,
-        },
-      },
     },
   });
 };
@@ -33,7 +37,7 @@ const register = (server) => {
 const plugin = {
   register,
   name: 'resize',
-  version: '0.3.0',
+  version: '1.0.0',
 };
 
 module.exports = { plugin };
