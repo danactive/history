443f70e965d1f8ea3b2f9207050b4bdfcbb7f942
danactive <dan@domaindesign.ca>
2020-02-23 16:57:17 -0800

    chore(Resize): Improve error message when resized images is missing

--- api/server/plugins/resize/test/resize.spec.js
+++ api/server/plugins/resize/test/resize.spec.js
@@ -1,5 +1,6 @@
 /* global JSON, require */
 const tape = require('tape-catch');
+const existsChecker = require('../../exists/lib/exists');
 
 tape('Verify resize library', { skip: false }, (describe) => {
   const calipers = require('calipers')('jpeg');
@@ -38,24 +39,48 @@ tape('Verify resize library', { skip: false }, (describe) => {
 
   describe.test('* Resize JPEG file to photo and thumb to parent folder', { skip: false }, async (assert) => {
     const originalRelativeFile = path.join(fixturesPath, 'originals/2016-07-12.jpg');
-    await plugin.resize(originalRelativeFile);
+    const originalAbsoluteFile = await utils.file.safePublicPath(originalRelativeFile);
 
     try {
-      const originalAbsoluteFile = await utils.file.safePublicPath(originalRelativeFile);
+      await existsChecker.pathExists(originalRelativeFile);
+
+      assert.ok(originalRelativeFile, `Original image file found at relative ${originalRelativeFile}`);
+
+      await plugin.resize(originalRelativeFile);
+
+      await existsChecker.pathExists(originalAbsoluteFile);
+      assert.ok(originalAbsoluteFile, `Original image file found at ${originalAbsoluteFile}`);
+    } catch (error) {
+      assert.fail(`Original resize failed ${error}`);
+    }
+
+    try {
       const photoPath = originalAbsoluteFile.replace(ORIGINAL_FOLDER_NAME, PHOTO_FOLDER_NAME);
 
-      const resultPhoto = await calipers.measure(photoPath);
-      assert.equal(resultPhoto.pages[0].width, photoDims.width, 'Photo width measured');
-      assert.ok(resultPhoto.pages[0].height <= photoDims.height, 'Photo height measured');
-      fs.unlink(photoPath, () => assert.pass('Clean up photo'));
+      await existsChecker.pathExists(photoPath);
+      assert.ok(originalRelativeFile, `Photo image file found at ${photoPath}`);
 
+      const resultPhoto = await calipers.measure(photoPath);
+      assert.equal(resultPhoto.pages[0].width, photoDims.width, `Photo width measured at ${photoDims.width}`);
+      assert.ok(resultPhoto.pages[0].height <= photoDims.height, `Photo height measured at ${photoDims.height}`);
+
+      fs.unlink(photoPath, () => assert.pass('Clean up photo'));
+    } catch (error) {
+      assert.fail(`Photo resize failed ${error}`);
+    }
+
+    try {
       const thumbPath = originalAbsoluteFile.replace(ORIGINAL_FOLDER_NAME, THUMB_FOLDER_NAME);
+
+      await existsChecker.pathExists(thumbPath);
+      assert.ok(originalRelativeFile, `Thumb image file found at ${thumbPath}`);
+
       const resultThumb = await calipers.measure(thumbPath);
       assert.equal(resultThumb.pages[0].width, thumbDims.width, 'Thumb width measured');
       assert.ok(resultThumb.pages[0].height <= thumbDims.height, 'Thumb height measured');
       fs.unlink(thumbPath, () => assert.pass('Clean up thumb'));
     } catch (error) {
-      assert.fail(`Resize failed ${error}`);
+      assert.fail(`Thumbnail resize failed ${error}`);
     }
 
     assert.end();
