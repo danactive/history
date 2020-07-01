600692d8db95dcb69bc298fba732067fa697922c
danactive <dan@domaindesign.ca>
2020-07-01 15:51:15 -0700

    feat(favicon): Update favicon to Sony Alpha 5100 camera

--- ui/internals/webpack/webpack.prod.babel.js
+++ ui/internals/webpack/webpack.prod.babel.js
@@ -8,6 +8,8 @@ const { HashedModuleIdsPlugin } = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
+const pkg = require('../../../api/package.json');
+
 module.exports = require('./webpack.base.babel')({
   mode: 'production',
 
@@ -112,9 +114,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: pkg.name,
+      short_name: pkg.name,
+      description: pkg.description,
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
