e267f87f140fda4e872841222902d70d19eb41b2
danactive <dan@domaindesign.ca>
2020-07-07 16:01:26 -0700

    fix(Home Page): Translate Host Storage text to Japanese and auto translate to German

--- ui/app/components/HostStorage/messages.js
+++ ui/app/components/HostStorage/messages.js
@@ -3,8 +3,16 @@ import { defineMessages } from 'react-intl';
 export const scope = 'app.components.HostStorage';
 
 export default defineMessages({
+  clearHeader: {
+    id: `${scope}.clearHeader`,
+    defaultMessage: 'Clear tokens from browser storage',
+  },
   instruction: {
     id: `${scope}.instruction`,
-    defaultMessage: '{host} token/path for loading gallery/media',
+    defaultMessage: '{host} token for loading photo gallery',
+  },
+  saveButton: {
+    id: `${scope}.saveButton`,
+    defaultMessage: 'Save in browser',
   },
 });
