49a9355b21fb8d31d394b10419207dbdfe2b71ec
danactive <dan@domaindesign.ca>
2021-07-24 20:13:57 -0700

    chore(Generic List): Restore components

---
+++ ui/app/components/GenericList/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import GenericList from '../index';
+
+describe('<GenericList />', () => {
+  test('Expect to have unit tests specified', () => {
+    expect(true).toEqual(true);
+  });
+});
