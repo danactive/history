e1e28c0d531ac60e934a8fced9cd1c718017b3bb
Dan BROOKS <dan@domaindesign.ca>
2021-08-06 15:27:46 -0700

    Use State for form fields

--- app/pages/twitter/search.jsx
+++ app/pages/twitter/search.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
+
 const Search = () => {
+  const [keyword, setKeyword] = useState('')
   const handleSubmit = (event) => {
     // TODO get the keyword from the input
-    console.log('User is searching for ')
+    console.log('User is searching for ', keyword)
     event.preventDefault()
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <h1>Twitter</h1>
-      <div>Search keyword <input type="text" /> <input type="submit" value="Search Twitter" /></div>
+      <div>Search keyword
+        <input type="text" onChange={(event) => setKeyword(event.target.value)} />
+        <input type="submit" value="Search Twitter" />
+      </div>
       <ul><li>Search results todo</li></ul>
     </form>
   )
