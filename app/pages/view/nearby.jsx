274ec6cab92c8715a976e63b513bb6a40691d308
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-08-23 19:39:09 -0700

    made image large - highlight not working yet

--- app/pages/view/nearby.jsx
+++ app/pages/view/nearby.jsx
@@ -1,6 +1,23 @@
 
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components'
+
+const ImageList = styled.ul`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const Pic = styled.img`
+  width: 100%;
+`;
+
+const ImageItem = styled.li`
+  width: 17%;
+`;
 
 const Nearby = () => {
   const router = useRouter();
@@ -8,32 +25,48 @@ const Nearby = () => {
   const { lat } = router.query;
 
   const [nearbyPhotos, setNearbyPhotos] = useState(null);
+  const [largePhoto, setLargePhoto] = useState(null);
+  // const [highlight, setHighlight] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
   const latitude = (lat) || '';
   const longitude = (lon) || '';
+
   const fetchPhotos = async () => {
-    const response = await fetch(`/api/flickr?lat=${latitude}&lon=${longitude}`);
-    const result = await response.json();
-    console.log(result)
-    setNearbyPhotos(result.photos.map((photo) => (
-    <li><img src={photo.path} alt="image" /></li>
-    )));
+    try {
+      const response = await fetch(`/api/flickr?lat=${latitude}&lon=${longitude}`);
+      const result = await response.json();
+      setNearbyPhotos(result.photos.map((photo, index) => (
+      <ImageItem key={index}>
+        <Pic src={photo.path} alt="image" onClick={() => { handleClick(photo.path) }} />
+      </ImageItem>
+      )));
+    } catch {
+      setErrorMsg('could not connect')
+    }
+  }
+
+  const handleClick = (path) => {
+    setLargePhoto(path)
   }
 
   useEffect(() => {
     fetchPhotos();
   }, []);
 
-  if (nearbyPhotos === null) {
-    return <h1>null</h1>
-  }
-
   return (
     <div>
       <button onClick={() => router.back()}>Click here to go back</button>
       <h1>Nearby</h1>
       <h2>{latitude}, {longitude}</h2>
-      <link rel="icon" href="/favicon.ico" />
-      <ul>{nearbyPhotos}</ul>
+      {/* <ImageList>{nearbyPhotos}</ImageList> */}
+      <img src={largePhoto} alt="image" />
+      {errorMsg && (
+        <div>{errorMsg}</div>
+      )}
+      {nearbyPhotos && (
+        <ImageList>{nearbyPhotos}</ImageList>
+      )}
     </div>
   )
 }
