270734f1a0c1d393fb388582ce8291d49d913a6d
danactive <dan@domaindesign.ca>
2023-07-30 11:51:40 -0700

    fix(Next > Loadable): Drop loadable due to underuse

--- next/src/components/GenericList/Loadable.tsx
+++
@@ -1,6 +0,0 @@
-import loadable from '../../utils/loadable'
-import LoadingIndicator from '../LoadingIndicator'
-
-export default loadable(() => import('./index'), {
-  fallback: <LoadingIndicator />,
-})
