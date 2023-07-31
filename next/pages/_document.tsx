1037fa1369c514d436695505a4043d7c96361c45
danactive <dan@domaindesign.ca>
2023-07-30 19:47:45 -0700

    feat(Next > MUI): Add MUI Joy UI

--- next/pages/_document.tsx
+++ next/pages/_document.tsx
@@ -8,7 +8,6 @@ export default class MyDocument extends Document {
 
     try {
       ctx.renderPage = () => originalRenderPage({
-        // eslint-disable-next-line react/jsx-props-no-spreading
         enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
       })
 
--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,9 +1,13 @@
-import Head from 'next/head'
+import '@fontsource/public-sans';
+import {List, ListDivider, ListItem} from '@mui/joy';
+import Head from 'next/head';
+import { CssVarsProvider } from '@mui/joy/styles';
+import styled from 'styled-components'
 
-import { get as getGalleries } from '../src/lib/galleries'
-import GenericList from '../src/components/GenericList'
-import ListItem from '../src/components/ListItem'
-import Link from '../src/components/Link'
+import GenericList from '../src/components/GenericList';
+import Link from '../src/components/Link';
+import ListItemOld from '../src/components/ListItem'
+import { get as getGalleries } from '../src/lib/galleries';
 
 export async function getStaticProps() {
   const { galleries } = await getGalleries()
@@ -17,23 +21,51 @@ export async function getStaticProps() {
 
 function ListComponent({ item }) {
   return (
-    <ListItem item={<Link href={`/${item.gallery}`}>{item.gallery}</Link>} />
+    <ListItemOld item={<Link href={`/${item.gallery}`}>{item.gallery}</Link>} />
   )
 }
 
+const Wrapper = styled.div`
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  background-color: #545454;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  overflow: hidden;
+`
+
 function Home({ galleries }) {
   return (
-    <div>
+    <>
       <Head>
         <title>History App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>List of Galleries</h1>
-        <GenericList loading={false} error={false} items={galleries} component={ListComponent} />
-      </main>
-    </div>
+      <CssVarsProvider defaultMode="dark">
+        <main>
+          <h1>List of Galleries</h1>
+          <Wrapper>
+            <List aria-labelledby="galleries" variant="outlined" sx={{
+                listStyle: 'none',
+                margin: 0,
+                padding: '0',
+                width: '100%',
+                maxHeight: '30em',
+                overflowY: 'auto',
+              }}>
+              <ListItem>1 red onion</ListItem>
+              <ListDivider sx={{ background: '#ccc' }} inset="gutter"/>
+              <ListItem>2 red peppers</ListItem>
+              <ListDivider sx={{ background: '#ccc' }} inset="gutter"/>
+              <ListItem>120g bacon</ListItem>
+            </List>
+          </Wrapper>
+          <GenericList loading={false} error={false} items={galleries} component={ListComponent} />
+        </main>
+      </CssVarsProvider>
+    </>
   )
 }
 
