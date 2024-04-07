36e1d86de8d7528252fa2c523a1a704ba89533a1
danactive <dan@domaindesign.ca>
2024-04-07 16:11:37 -0700

    Rename API as Next CI detects a name conflict?

--- api/server/plugins/album/components/page.jsx
+++ api/server/plugins/album/components/page.jsx
@@ -14,7 +14,7 @@ function Page({ album }) {
         <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
         <link href="https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css" rel="stylesheet" />
         <link href="./album/static/album-styles.css" rel="stylesheet" />
-        <link href="./album/static/map.css" rel="stylesheet" />
+        <link href="./album/static/map-styles.css" rel="stylesheet" />
       </head>
       <body>
         <div id="divToolbox">
