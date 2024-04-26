1d81887f234ad64fe449a1d61d7ad3cd669a8de7
danactive <dan@domaindesign.ca>
2024-04-26 09:13:00 -0700

    SplitViewer to CSS Modules

--- src/components/SplitViewer/index.tsx
+++ src/components/SplitViewer/index.tsx
@@ -1,41 +1,26 @@
 import Color from 'color-thief-react'
-import React, { useContext, useRef, type LegacyRef } from 'react'
+import {
+  useContext, useRef, type LegacyRef, type ReactNode,
+} from 'react'
 import ImageGallery, { type ReactImageGalleryItem, type ReactImageGalleryProps } from 'react-image-gallery'
 import 'react-image-gallery/styles/css/image-gallery.css'
 import type { MapRef } from 'react-map-gl'
-import styled from 'styled-components'
 
 import config from '../../../config.json'
+import { Viewed } from '../../hooks/useMemory'
+import { Item } from '../../types/common'
 import { getExt } from '../../utils'
 import AlbumContext from '../Context'
 import SlippyMap from '../SlippyMap'
-import Video from '../Video'
-
-import { Viewed } from '../../hooks/useMemory'
-import { Item } from '../../types/common'
 import { validatePoint } from '../SlippyMap/options'
-
-const Split = styled.section`
-  display: grid;
-  grid-template-columns: 60% 40%;
-  grid-template-areas: 'left right';
-`
-
-const Left = styled.section`
-  grid-area: left;
-  height: 80vh;
-`
-
-const Right = styled.section`
-  grid-area: right;
-  height: 80vh;
-`
+import Video from '../Video'
+import styles from './styles.module.css'
 
 interface ImageGalleryType extends ReactImageGalleryItem {
   filename: string;
   mediaPath: string;
   caption: string;
-  renderItem?(item: ReactImageGalleryItem & { caption: string; mediaPath: string; }): React.ReactNode;
+  renderItem?(item: ReactImageGalleryItem & { caption: string; mediaPath: string; }): ReactNode;
 }
 
 const toCarousel = (item: Item) => {
@@ -125,8 +110,8 @@ function SplitViewer({
           </style>
         )}
       </Color>
-      <Split className="image-gallery-background">
-        <Left key="splitLeft">
+      <section className={`${styles.split} image-gallery-background`}>
+        <section className={styles.left} key="splitLeft">
           <ImageGallery
             ref={refImageGallery}
             onBeforeSlide={handleBeforeSlide}
@@ -138,12 +123,12 @@ function SplitViewer({
             useWindowKeyDown={false}
             lazyLoad
           />
-        </Left>
-        <Right key="splitRight" ref={refMapBox}>
+        </section>
+        <section className={styles.right} key="splitRight" ref={refMapBox}>
           <SlippyMap mapRef={mapRef} items={items} centroid={items[memoryIndex]} />
           <button type="button" onClick={fullscreenMap}>Full Map</button>
-        </Right>
-      </Split>
+        </section>
+      </section>
     </>
   )
 }
