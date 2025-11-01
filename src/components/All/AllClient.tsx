253422ab0cb0c77c47b8b685a8c5622d76d0f9f3
danactive <dan@domaindesign.ca>
2025-10-31 20:15:01 -0700

    fix tsc

--- src/components/All/AllClient.tsx
+++ src/components/All/AllClient.tsx
@@ -9,6 +9,7 @@ import AlbumContext from '../Context'
 import SplitViewer from '../SplitViewer'
 import AllItems from './Items'
 import useMapFilter from '../../hooks/useMapFilter'
+import type { ServerSideAllItem } from '../../types/common'
 
 export default function AllClient({ items, indexedKeywords }: All.ComponentProps) {
   const zooms = useMemo(() => ({ geo: { zoom: config.defaultZoom } }), [])
@@ -27,6 +28,16 @@ export default function AllClient({ items, indexedKeywords }: All.ComponentProps
     itemsToShow,
   } = useMapFilter({ items, indexedKeywords })
 
+  const itemsWithCorpus: ServerSideAllItem[] = useMemo(
+    () => itemsToShow.map(i => ({
+      // ensure required corpus & coordinateAccuracy satisfy ServerSideAllItem
+      corpus: (i as any).corpus ?? '',
+      ...i,
+      coordinateAccuracy: i.coordinateAccuracy ?? 0,
+    })),
+    [itemsToShow],
+  )
+
   return (
     <div>
       <AlbumContext.Provider value={zooms}>
@@ -42,7 +53,7 @@ export default function AllClient({ items, indexedKeywords }: All.ComponentProps
           onToggleMapFilter={handleToggleMapFilter}
           onMapBoundsChange={handleBoundsChange}
         />
-        <AllItems items={itemsToShow} keyword={keyword} refImageGallery={refImageGallery} />
+        <AllItems items={itemsWithCorpus} keyword={keyword} refImageGallery={refImageGallery} />
       </AlbumContext.Provider>
     </div>
   )
